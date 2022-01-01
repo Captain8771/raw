82f43312eb6be4203e6119eafd8859fa3d7732cb
The Captain <81252038+Captain8771@users.noreply.github.com>
2022-01-01 11:34:37 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Plugin } = require('powercord/entities');
+
+module.exports = class Template extends Plugin {
+    startPlugin() {
+        return;
+    }
+    pluginWillUnload() {
+        return;
+    }
+};
